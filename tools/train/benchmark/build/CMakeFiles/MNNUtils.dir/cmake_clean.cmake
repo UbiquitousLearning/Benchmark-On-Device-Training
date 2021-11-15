7c9422237865cd0d8d02a8202eda2e5e5cff8282
cdq <bupt_yjl@163.com>
2021-11-15 16:18:03 +0800

    delete directory tools/train/benchmark/build to remove local cache and save storage.

--- tools/train/benchmark/build/CMakeFiles/MNNUtils.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/MNNUtils.dir/source/utils/InitNet.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/MNNUtils.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
